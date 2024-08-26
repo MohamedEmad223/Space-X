9957e9fd85cbafa472f95b9ee2e1558c612bc42e
mohamed emad <mohamedelbakrawy70@gmail.com>
2024-08-26 18:21:54 +0300

    - finish repo

---
+++ lib/feature/crew/data/repo/crew_repo.dart
@@ -0,0 +1,25 @@
+import 'package:either_dart/either.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:space_xplore/core/networking/api_services.dart';
+import 'package:space_xplore/feature/crew/data/model/query_crew_model.dart';
+
+import '../../../../core/networking/api_error_handler.dart';
+
+class CrewRepo {
+  final ApiServices _apiServices;
+
+  CrewRepo(this._apiServices);
+
+  Future<Either<ApiErrorHandler, QueryCrewModel>> fetchAllCrewMembers(
+      {required int page}) async {
+    try {
+      final crewMembers = await _apiServices.fetchAllCrewMembers({
+        'page': page,
+        'limit': 10,
+      });
+      return Right(crewMembers);
+    } catch (e) {
+      return Left(ServerFailure(errorMessage: e.toString()));
+    }
+  }
+}
