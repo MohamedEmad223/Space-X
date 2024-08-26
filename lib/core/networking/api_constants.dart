024ee8fc57ce081027267d19ea3b6d7670ce2729
mohamed emad <mohamedelbakrawy70@gmail.com>
2024-08-26 18:28:05 +0300

    - add crew to constants file

--- lib/core/networking/api_constants.dart
+++ lib/core/networking/api_constants.dart
@@ -1,5 +1,6 @@
-class ApiConstants{
+class ApiConstants {
   static const String baseUrl = "https://api.spacexdata.com/v4/";
   static const String rockets = "rockets";
   static const String companyInfo = "company";
+  static const String crew = "crew";
 }
