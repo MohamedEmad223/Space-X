f42127610266f024fd3870c3e2999bf81a5899f2
mohamed emad <mohamedelbakrawy70@gmail.com>
2024-08-27 21:35:47 +0300

    finish feature/launches-ui

---
+++ lib/feature/launches/view/screens/launch_details_screen.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../core/helpers/spacing.dart';
+import '../../../../core/helpers/text_helper.dart';
+import '../../../../core/theming/text_style.dart';
+import '../../../../core/widgets/background_container.dart';
+import '../../../../core/widgets/custom_app_bar.dart';
+import '../../../../core/widgets/custom_text_span.dart';
+import '../../../../core/widgets/link_text.dart';
+
+class LaunchDetailsScreen extends StatelessWidget {
+  const LaunchDetailsScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: const CustomAppBar(),
+      body: BackgroundContainer(
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsetsDirectional.only(
+              top: 13.h,
+              bottom: 13.h,
+              start: 16.w,
+              end: 16.w,
+            ),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  verticalSpace(30),
+                  Center(
+                    child: Text(
+                      'vgjh',
+                      style: TextStyles.font24WhiteBoldOrbitron,
+                    ),
+                  ),
+                  verticalSpace(25),
+                  const CustomTextSpan(
+                    textTitle: Constants.launchDetailsAttribute,
+                    textDescription: 'fghjfj',
+                  ),
+                  verticalSpace(20),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CustomTextSpan(
+                          textTitle: Constants.launchDateAttribute,
+                          textDescription: 'ch'),
+                      CustomTextSpan(
+                        textTitle: Constants.launchFlightNumAttribute,
+                        textDescription: 'fjfgy',
+                      ),
+                    ],
+                  ),
+                  verticalSpace(20),
+                  const CustomTextSpan(
+                      textTitle: Constants.launchSuccessAttribute,
+                      textDescription: 'cg'),
+                  verticalSpace(20),
+                  const CustomTextSpan(
+                      textTitle: Constants.launchFailuresAttribute,
+                      textDescription: 'hgj'),
+                  verticalSpace(20),
+                  const LinkText(
+                    linkUrl: 'ygu',
+                    linkName: 'fgjhhj',
+                  ),
+                  verticalSpace(20),
+                  const LinkText(
+                    linkUrl: 'fgh',
+                    linkName: 'gfh',
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/feature/launches/view/widgets/launch_custom_card.dart
+++ lib/feature/launches/view/widgets/launch_custom_card.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
+import 'package:space_xplore/feature/launches/view/screens/launch_details_screen.dart';
 
 import '../../../../core/theming/colors_manger.dart';
 import '../../../../core/theming/text_style.dart';
@@ -9,37 +11,45 @@ class LaunchCustomCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 150.w,
-      height: 150.h,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8.r),
-        color: ColorsManager.cardColor.withOpacity(0.1),
-        boxShadow: [
-          BoxShadow(
-            color: ColorsManager.shadowColor,
-            blurRadius: 5.r,
-            offset: Offset(5.w, 5.h),
-          ),
-          BoxShadow(
-            color: ColorsManager.shadowColor,
-            blurRadius: 10.r,
-            offset: Offset(5.w, 5.h),
-          ),
-        ],
-      ),
-      child: Padding(
-        padding: EdgeInsets.only(top: 15.w, bottom: 10.w),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('assets/images/launch.png.png'),
-            const Spacer(),
-            Text(
-              'data',
-              style: TextStyles.font16WhiteRegularOrienta,
-            )
+    return GestureDetector(
+      onTap: () {
+        PersistentNavBarNavigator.pushNewScreen(context,
+            withNavBar: false,
+            pageTransitionAnimation: PageTransitionAnimation.sizeUp,
+            screen: const LaunchDetailsScreen());
+      },
+      child: Container(
+        width: 150.w,
+        height: 150.h,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8.r),
+          color: ColorsManager.cardColor.withOpacity(0.1),
+          boxShadow: [
+            BoxShadow(
+              color: ColorsManager.shadowColor,
+              blurRadius: 5.r,
+              offset: Offset(5.w, 5.h),
+            ),
+            BoxShadow(
+              color: ColorsManager.shadowColor,
+              blurRadius: 10.r,
+              offset: Offset(5.w, 5.h),
+            ),
           ],
+        ),
+        child: Padding(
+          padding: EdgeInsets.only(top: 15.w, bottom: 10.w),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/launch.png.png'),
+              const Spacer(),
+              Text(
+                'data',
+                style: TextStyles.font16WhiteRegularOrienta,
+              )
+            ],
+          ),
         ),
       ),
     );
